aecf91c6f77dd4a18ea963c60ff6eb966e52b761
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2021-05-07 14:13:35 -0300

    Inclusão dos jogos

---
+++ src/main/java/ec/ftt/api/GameApi.java
@@ -0,0 +1,114 @@
+package ec.ftt.api;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import ec.ftt.dao.GameDao;
+import ec.ftt.model.Game;
+
+@WebServlet("/game")
+public class GameApi extends HttpServlet  {
+	private static final long serialVersionUID = 2L;
+
+	public GameApi() {
+		super();
+	}
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	public void destroy() {
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setStatus(418);
+
+		String gameId = request.getParameter("game-id");
+
+		if(gameId != null) {
+			long id = Long.valueOf(gameId);
+
+			GameDao gameDao = new GameDao();
+
+			Game game = gameDao.getGameById(id);
+			Gson gson = new Gson();
+			response.getWriter().append(gson.toJson(game));
+
+		} else {
+
+			GameDao gameDao = new GameDao();
+
+			List<Game> games = gameDao.getAllGame();
+
+			Gson gson = new Gson();
+
+			response.getWriter().append(gson.toJson(games));
+		} 
+
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Game u = new Game(
+				request.getParameter("game-name"),
+				request.getParameter("game-producer"),
+				request.getParameter("game-genre"),
+				request.getParameter("game-releaseDate")
+				);
+
+		GameDao gameDao = new GameDao();
+
+		gameDao.addGame(u);
+
+		response.getWriter().append(u.toString());
+
+	}
+
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		Game u = new Game(
+				request.getParameter("game-name"),
+				request.getParameter("game-producer"),
+				request.getParameter("game-genre"),
+				request.getParameter("game-releaseDate")
+				);
+		GameDao gameDao = new GameDao();
+
+		gameDao.updateGame(u);
+
+		response.getWriter().append(u.toString());
+
+
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setStatus(418);
+
+		if (request.getParameter("game-id") == null)
+			response.sendError(407, "Informe o ID do usuário a ser retornado!!!" );
+		else {
+			Long gameId = Long.valueOf(request.getParameter("game-id"));
+
+
+
+			GameDao ud = new GameDao();
+
+			ud.deleteGame(gameId);
+
+			response.getWriter().append(request.getParameter("game-id") + " Game removido");
+		}
+	}
+
+}
